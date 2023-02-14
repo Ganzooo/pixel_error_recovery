b2c16616bea9be78a9bce83e787b971ed36c0229
Ganzoo <gnzrg25@gmail.com>
2023-02-14 16:09:24 +0900

    -- update script_0

--- script_train_hybrid_server.sh
+++ script_train_hybrid_server.sh
@@ -1,2 +1,2 @@
 #python train_hybrid_server.py train_config.description=hybrid_ep500 train_config.epochs=500 train_config.gpu_id=0
-python train_hybrid_server.py train_config.description=hybrid_ep5000_ml1 train_config.epochs=5000 train_config.gpu_id=0 train_config.use_masked_loss=True train_config.batch_size=256
+python train_hybrid_server.py train_config.description=hybrid_ep5000_l1 train_config.epochs=5000 train_config.gpu_id=0 train_config.batch_size=256
