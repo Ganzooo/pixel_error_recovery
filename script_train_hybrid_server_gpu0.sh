2be5cd94771c428493d211455d5424fc91af3fcd
문병일 <dotorl127@gmail.com>
2024-02-29 12:28:07 +0900

    add pixel shuffle plain detect test shell script

--- script_train_hybrid_server_gpu0.sh
+++ script_train_hybrid_server_gpu0.sh
@@ -1,3 +1,3 @@
 #python train_detect_pixel.py --multirun hydra.sweep.dir=trained/module hydra.sweep.subdir=6 model.num_module=6 train_config.gpu_id=0
 #python train_detect_pixel.py --multirun hydra.sweep.dir=trained/module hydra.sweep.subdir=8  model.num_module=8 train_config.gpu_id=0
-python train_detect_pixel.py --multirun hydra.sweep.dir=multirun/ps train_config.gpu_id=0
+python train_detect_pixel.py --multirun hydra.sweep.dir=multirun/ps hydra.sweep.subdir= train_config.gpu_id=0
